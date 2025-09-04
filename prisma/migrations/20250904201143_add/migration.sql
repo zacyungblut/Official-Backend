c8de37e4af8abc60a3a848a398fcb7d9254a4a65
dragochar <zacyungblut@gmail.com>
2025-09-04 17:37:35 -0400

    reset migrations and add better, newer user schema that includes more relationship status and implies users own individual status

--- prisma/migrations/20250904201143_add/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN     "verified" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20250904213657_init/migration.sql
+++ prisma/migrations/20250904213657_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "public"."Status" AS ENUM ('SINGLE', 'RELATIONSHIP', 'SITUATIONSHIP');
+CREATE TYPE "public"."Status" AS ENUM ('DATING', 'ENGAGED', 'MARRIED', 'SEPARATED', 'WIDOWED', 'SITUATIONSHIP', 'FRIENDS_WITH_BENEFITS', 'ON_A_BREAK', 'OPEN_RELATIONSHIP', 'POLYAMOROUS');
 
 -- CreateEnum
 CREATE TYPE "public"."InviteStatus" AS ENUM ('PENDING', 'ACCEPTED', 'DECLINED', 'EXPIRED', 'CANCELLED');
@@ -9,12 +9,24 @@ CREATE TABLE "public"."User" (
     "id" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "name" TEXT,
-    "relationshipStatus" "public"."Status" NOT NULL DEFAULT 'SINGLE',
     "verificationCode" TEXT,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Relationship" (
+    "id" TEXT NOT NULL,
+    "status" "public"."Status" NOT NULL DEFAULT 'DATING',
+    "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Relationship_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -31,11 +43,11 @@ CREATE TABLE "public"."Invite" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."_UserPartners" (
+CREATE TABLE "public"."_RelationshipToUser" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL,
 
-    CONSTRAINT "_UserPartners_AB_pkey" PRIMARY KEY ("A","B")
+    CONSTRAINT "_RelationshipToUser_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateIndex
@@ -45,13 +57,13 @@ CREATE UNIQUE INDEX "User_phone_key" ON "public"."User"("phone");
 CREATE UNIQUE INDEX "Invite_senderPhone_recipientPhone_key" ON "public"."Invite"("senderPhone", "recipientPhone");
 
 -- CreateIndex
-CREATE INDEX "_UserPartners_B_index" ON "public"."_UserPartners"("B");
+CREATE INDEX "_RelationshipToUser_B_index" ON "public"."_RelationshipToUser"("B");
 
 -- AddForeignKey
 ALTER TABLE "public"."Invite" ADD CONSTRAINT "Invite_senderPhone_fkey" FOREIGN KEY ("senderPhone") REFERENCES "public"."User"("phone") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."_UserPartners" ADD CONSTRAINT "_UserPartners_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."_RelationshipToUser" ADD CONSTRAINT "_RelationshipToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Relationship"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."_UserPartners" ADD CONSTRAINT "_UserPartners_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."_RelationshipToUser" ADD CONSTRAINT "_RelationshipToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
