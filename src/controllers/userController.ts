6838ba2bf08e11e469bbd3581ef4dc9cd822ce33
dragochar <zacyungblut@gmail.com>
2025-09-03 21:30:15 -0400

    initial commit

---
+++ src/controllers/userController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getUser(req: Request, res: Response) {
+  const { phone } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { phone },
+  });
+  res.json(user);
+}
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import userRoutes from "./routes/userRoutes";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.get("/", (_req, res) => {
+  res.send("API is running 🚀");
+});
+
+const PORT = process.env["PORT"] || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
