07031a23a1dcdd8773ddc9e336a81c2545820353
dragochar <zacyungblut@gmail.com>
2025-09-05 09:52:40 -0400

    feat: new ecosystem file for live backend

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "official-backend",
+      script: "./dist/server.js",
+      cwd: "/home/ec2-user/Official-Backend",
+      env: {
+        NODE_ENV: "production",
+        PORT: "4000",
+      },
+      max_memory_restart: "2G",
+      kill_timeout: 3000,
+      wait_ready: true,
+      listen_timeout: 10000,
+      node_args: "--max-old-space-size=2048",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_restarts: 10,
+      min_uptime: "5s",
+    },
+  ],
+};
